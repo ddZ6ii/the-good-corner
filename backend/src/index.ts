1502113351cd11df8d493848b8e13821a03ae399
Denis Dezest <denis.dezest@gmail.com>
2024-09-24 17:02:40 +0200

    ✨ Create api server.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,10 @@
-console.log('Hello, World!');
+import express from 'express';
+import 'dotenv/config';
+
+const API_PORT = parseInt(process.env.API_PORT ?? '3000', 10);
+const app = express();
+
+// Server.
+app.listen(API_PORT, () => {
+  console.log(`Server is running on port ${API_PORT.toString()}`);
+});
