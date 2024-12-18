ced4ce9b69ede3d105ac0ea42dbd7c29363d4b4e
Denis Dezest <denis.dezest@gmail.com>
2024-12-18 17:57:51 +0100

    🏗️ Replace sqlite by postgresql.

--- backend/src/migrate.ts
+++ backend/src/migrate.ts
@@ -1,26 +1,26 @@
-import fs from 'fs';
-import sqlite3 from 'sqlite3';
-import chalk from 'chalk';
-import { resolve } from 'node:path';
-import { dbFileUrl } from '@/db.config';
+// import fs from 'fs';
+// import sqlite3 from 'sqlite3';
+// import chalk from 'chalk';
+// import { resolve } from 'node:path';
+// import { dbFileUrl } from '@/db.config';
 
-const dumpFileUrl = resolve(import.meta.dirname, 'dump.sql');
+// const dumpFileUrl = resolve(import.meta.dirname, 'dump.sql');
 
-const db = new sqlite3.Database(dbFileUrl);
+// const db = new sqlite3.Database(dbFileUrl);
 
-const sql = fs.readFileSync(dumpFileUrl, {
-  encoding: 'utf8',
-});
+// const sql = fs.readFileSync(dumpFileUrl, {
+//   encoding: 'utf8',
+// });
 
-// Reset database content from sql dump file.
-db.exec(sql, (err) => {
-  if (err) {
-    console.error(
-      chalk.red('Failed to migrate database from dump.sql file!'),
-      err,
-    );
-  }
-});
+// // Reset database content from sql dump file.
+// db.exec(sql, (err) => {
+//   if (err) {
+//     console.error(
+//       chalk.red('Failed to migrate database from dump.sql file!'),
+//       err,
+//     );
+//   }
+// });
 
-// Prevent deleting a parent row if there are dependent rows in the child table.
-db.run('PRAGMA foreign_keys = ON');
+// // Prevent deleting a parent row if there are dependent rows in the child table.
+// db.run('PRAGMA foreign_keys = ON');
